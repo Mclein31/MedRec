088bad55b025a22564540e163fd5e29b7fad9e85
McLein <mcleinonza0531@gmail.com>
2026-06-24 11:43:12 +0800

    added the basics for the log-in (not finished)

---
+++ mobile/app/(tabs)/login.tsx
@@ -0,0 +1,60 @@
+import { View, Text, TextInput, Button } from 'react-native';
+import { useState } from 'react';
+import { FlipInEasyX } from 'react-native-reanimated';
+
+export default function LoginScreen() {
+  // state for inputs
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <View style={{
+      padding: 40,
+      gap: 12, 
+      // flex: 1,
+      // justifyContent: 'center',
+      // alignItems:'center'
+      }}>
+
+
+
+      {/* Email input */}
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        keyboardType="email-address"
+        style={{
+          borderWidth: 1,
+          borderColor: '#ccc',
+          padding: 10,
+          borderRadius: 8,
+          alignContent: 'center'
+        }}
+      />
+
+      {/* Password input */}
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={{
+          borderWidth: 1,
+          borderColor: '#ccc',
+          padding: 10,
+          borderRadius: 8,
+        }}
+      />
+
+      {/* Button */}
+      <Button title="Log In" onPress={handleLogin} />
+    </View>
+  );
+}
